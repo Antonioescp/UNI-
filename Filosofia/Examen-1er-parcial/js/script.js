e551e48b3242e6a2954771ac0dfe7db4f3938986
Antonio Escorcia <antonioescorciap@gmail.com>
2020-05-17 21:49:39 -0600

    Funcionalidad para viewports diferentes

--- Filosofia/Examen-1er-parcial/js/script.js
+++ Filosofia/Examen-1er-parcial/js/script.js
@@ -7,41 +7,41 @@ document.addEventListener("DOMContentLoaded", function (event){
 	$ajaxUtils.realizarPeticionGet("snippets/home-tiles.html", function (peticion) {
 		qs("#main-content").innerHTML = peticion;
 
+		// tile1 content
+		qs("#tile1").onclick = tile1Click;
+
 		// tile2 content
 		qs("#tile2").onclick = tile2Click;
-		qs("#tile2").blur = tile2Blur;
 
 		//tile3 content
 		qs("#tile3").onclick = tile3Click;
-		qs("#tile2").blur = tile3Blur;
 
 	}, false);
 
+	// til1 function 
+	function tile1Click (event){
+		console.log("not yet set");
+	}
+
+	// tile 2 function
 	function tile2Click (event) {
 		$ajaxUtils.realizarPeticionGet("snippets/tile2-content.html",
 		function(peticion){
 				qs("#tile2_target").innerHTML = peticion;
-				qs("#tile2").onclick = tile2Blur;
+				qs("#common_target_lg").innerHTML = peticion;
+				qs("#t2-md-target").innerHTML = peticion;
 		}
 		,false);
 	}
 
-	function tile2Blur (event){
-		qs("#tile2_target").innerHTML = "";
-		qs("#tile2").onclick = tile2Click;
-	}
-
+	// tile 3 function
 	function tile3Click (event) {
 		$ajaxUtils.realizarPeticionGet("snippets/tile3-content.html",
 		function(peticion){
 				qs("#tile3_target").innerHTML = peticion;
-				qs("#tile3").onclick = tile3Blur;
+				qs("#common_target_lg").innerHTML = peticion;
+				qs("#tile3_target_md").innerHTML = peticion;
 		}
 		,false);
-	}
-
-	function tile3Blur (event){
-		qs("#tile3_target").innerHTML = "";
-		qs("#tile3").onclick = tile3Click;
 	}
 });
